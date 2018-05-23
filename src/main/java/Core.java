38b4f94f2d1f6af1390d00459decd973f0a27fd8
myreage <brassenslebg@gmail.com>
2018-05-24 00:01:51 +0200

    lalala bit

--- src/main/java/Core.java
+++ src/main/java/Core.java
@@ -3,6 +3,7 @@ import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.params.RegTestParams;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.script.Script;
+import org.bitcoinj.wallet.KeyChain;
 import org.bitcoinj.wallet.Wallet;
 import org.bitcoinj.wallet.listeners.KeyChainEventListener;
 import org.bitcoinj.wallet.listeners.ScriptsChangeEventListener;
@@ -10,7 +11,10 @@ import org.bitcoinj.wallet.listeners.WalletCoinsReceivedEventListener;
 import org.bitcoinj.wallet.listeners.WalletCoinsSentEventListener;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 import org.bitcoinj.core.listeners.TransactionConfidenceEventListener;
 
@@ -20,13 +24,27 @@ import org.bitcoinj.core.listeners.TransactionConfidenceEventListener;
  *
  * In this example we also define a WalletEventListener class with implementors that are called when the wallet changes (for example sending/receiving money)
  */
-public class Test {
+public class Core {
+
 
     public static void main(String[] args) {
 
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "params.properties";
 
-        WalletAppKit kit = new WalletAppKit(params, new File("~/.bitcoin/regtest/wallets"), "walletappkit-example");
+        Properties appProps = new Properties();
+        try {
+            appProps.load(new FileInputStream(appConfigPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String walletPath  = appProps.getProperty("walletPath");
 
+        NetworkParameters params = RegTestParams.get();
+
+
+
+        WalletAppKit kit = new WalletAppKit(params, new File(walletPath), "walletappkit-example");
 
 
         kit.connectToLocalHost();
@@ -39,6 +57,8 @@ public class Test {
             public void onCoinsReceived(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
                 System.out.println("-----> coins resceived: " + tx.getHashAsString());
                 System.out.println("received: " + tx.getValue(wallet));
+                System.out.println(wallet.getBalance(Wallet.BalanceType.ESTIMATED).toFriendlyString());
+
             }
         });
 
@@ -72,8 +92,23 @@ public class Test {
             }
         });
 
+        kit.wallet().addWatchedAddress(kit.wallet().freshReceiveAddress());
+        System.out.println("New address created");
+        List<Address> list = kit.wallet().getWatchedAddresses();
+        System.out.println("You have " + list.size() + " addresses!");
+        for (Address a: list) {
+            System.out.println(a.toString());
+        }
 
-        System.out.println("send money to: " + kit.wallet().freshReceiveAddress().toString());
+
+        while(true){
+
+        }
+
+
+
+
+
 
         // Make sure to properly shut down all the running services when you manually want to stop the kit. The WalletAppKit registers a runtime ShutdownHook so we actually do not need to worry about that when our application is stopping.
         //System.out.println("shutting down again");
